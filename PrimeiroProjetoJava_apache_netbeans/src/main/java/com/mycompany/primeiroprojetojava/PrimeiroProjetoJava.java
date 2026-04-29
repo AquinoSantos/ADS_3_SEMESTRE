e700f9e49d3cd031ef440b3f58f6db6c7d9993be
AquinoSantos <aquinisantiago@gmail.com>
2026-04-29 19:40:29 +0000

    Salvando projeto apache netbeans

---
+++ PrimeiroProjetoJava_apache_netbeans/src/main/java/com/mycompany/primeiroprojetojava/PrimeiroProjetoJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primeiroprojetojava;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class PrimeiroProjetoJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
